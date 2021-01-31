22bc4e7c557b3968b6c0e9dfac249e0e363ad7be
Svetlana Vassileva <72747194+svetlilaloli@users.noreply.github.com>
2021-01-31 11:57:22 +0000

    Add files via upload

---
+++ HTML.cs
@@ -0,0 +1,49 @@
+﻿using HTML.ClassLibrary;
+using System;
+
+namespace HTML
+{
+    class HTML
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+            string comment = Console.ReadLine();
+            Article article = new Article
+            {
+                Title = title,
+                Content = content
+            };
+
+            while (comment != "end of comments")
+            {
+                article.Comments.Add(comment);
+                comment = Console.ReadLine();
+            }
+            PrintInHTML(article);
+        }
+
+        private static void PrintInHTML(Article article)
+        {
+            if (article.Title != null)
+            {
+                Console.WriteLine("<h1>");
+                Console.WriteLine($"\t{article.Title}");
+                Console.WriteLine("</h1>");
+            }
+            if (article.Content != null)
+            {
+                Console.WriteLine("<article>");
+                Console.WriteLine($"\t{article.Content}");
+                Console.WriteLine("</article>");
+            }
+            foreach (string comment in article.Comments)
+            {
+                Console.WriteLine("<div>");
+                Console.WriteLine($"\t{comment}");
+                Console.WriteLine("</div>");
+            }
+        }
+    }
+}
